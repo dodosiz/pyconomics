92b8f2219e6966ee7e231ae37bfd8eb884018336
Theo Aswestopoulos <t.v.asvestopoulos@gmail.com>
2020-04-18 19:51:57 +0200

    geometrical deprecations

--- model.py
+++ model.py
@@ -1,31 +1,59 @@
 import matplotlib.pyplot as pl
 
-class BaseGraph:
+class SingleGraph:
     """
-    A base class to create objects for graphical representations.
+    A base class to create objects for graphical representations of a single plot.
     Attributes:
         x_data - an array with numbers for the x-axis
         y_data - an array with numbers for the y-axis
-        x_name(optional) - a name for the x-axis
-        y_name(optional) - a name for the y-axid
     """
-    def __init__(self, x_data, y_data, x_name = "", y_name = ""):
+    def __init__(self, x_data, y_data, title):
         self.x_data = x_data
         self.y_data = y_data
-        self.x_name = x_name
-        self.y_name = y_name
+        self.title = title
     
-    def render(self, mode):
+    def render(self, x_name = "", y_name = ""):
         """
-        Renders the graph based on the difined data for x and y axis.
+        Renders a single graph.
         Attributes:
-            mode the representation of the graph for example
-            'r-' red and linear
-            'bo' blue and with circles on the data points
-            'r--' red and dashed
+            x_name(optional) - a name for the x-axis
+            y_name(optional) - a name for the y-axid
         """
         fig, ax = pl.subplots()
-        ax.plot(self.x_data, self.y_data, mode)
-        ax.set_xlabel(self.x_name)
-        ax.set_ylabel(self.y_name)
+        ax.plot(self.x_data, self.y_data)
+        ax.set_xlabel(x_name)
+        ax.set_ylabel(y_name)
+        ax.grid(True)
+        ax.set_title(self.title)
         pl.show()
+
+class DualGraph:
+    """
+    A class to create objects for graphical representations with two plots.
+    Attributes:
+        plot_1 a single graph object for the first plot
+        plot_2 a single graph object for the second plot
+    """
+    def __init__(self, plot_1, plot_2, title):
+        self.plot_1 = plot_1
+        self.plot_2 = plot_2
+        self.title = title
+        
+    def render(self, x_name = "", y_name = ""):
+        """
+        Renders two graphs together.
+        Attributes:
+            x_name(optional) - a name for the x-axis
+            y_name(optional) - a name for the y-axid
+        """
+        fig, ax = pl.subplots()
+        ax.plot(self.plot_1.x_data, self.plot_1.y_data, label=self.plot_1.title)
+        ax.plot(self.plot_2.x_data, self.plot_2.y_data, label=self.plot_2.title)
+        ax.set_xlabel(x_name)
+        ax.set_ylabel(y_name)
+        ax.grid(True)
+        ax.set_title(self.title)
+        ax.legend()
+        pl.show()
+        
+        
--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import sys
 import numpy as np
-from model import BaseGraph
+from model import SingleGraph, DualGraph
 
-def linear_deprecation(a, m):
+def create_linear_deprecation(a, m):
     """
-    Plots the linear deprecation of a value.
+    Creates the single graph plot for the linear deprecation of a value.
     Attributes:
         a - the starting value (euro)
         m - the total years of use
@@ -12,11 +12,26 @@ def linear_deprecation(a, m):
     d = a / m
     x = np.arange(m + 1)
     y = a - d * x
-    graph = BaseGraph(x, y, "years", "value in euro")
-    graph.render("bo")
+    return SingleGraph(x, y, "linear deprecation")
+    
+def create_geometrical_deprecation(a, m, q):
+    """
+    Creates the single graph plot for the geometrical deprecation of a value.
+    Attributes:
+        a - the starting value (euro)
+        m - the total years of use
+        q - the deprecation quote
+    """
+    x = np.arange(m + 1)
+    y = a * (1 - q) ** x
+    return SingleGraph(x, y, "geometrical deprecation")
 
 if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[2])
-    linear_deprecation(a, b)
+    c = float(sys.argv[3])
+    ld = create_linear_deprecation(a, b)
+    gd = create_geometrical_deprecation(a, b, c)
+    dg = DualGraph(ld, gd, "linear and geometrical deprecation")
+    dg.render("years", "euro")
     
